7573e21c17008a18e922c79c6c1d79038066513a
Matthias Van Gestel <maregt0@gmail.com>
2013-03-15 01:58:04 +0100

    Add XML capabilities to mapper

--- mapper.py
+++ mapper.py
@@ -6,45 +6,93 @@
 #####################################
 import time
 # drawing on canvas
-from tkinter import *
+import tkinter as tk
 import copy
+# xml
+import xml.etree.ElementTree as etree
 
+###############XML#########################
+def get_location(element):
+	x = int(element.find("x").text)
+	y = int(element.find("y").text)
+	return (x,y)
+
+def parse_connection(element):
+	No_Connection = (None,None)
+	if element is None: 
+		element = No_Connection
+	else:
+		element = get_location(element)
+	return element
+
+
+def parse_mapping_xml():
+	tree = etree.parse('map.xml')
+	root = tree.getroot()
+
+	map = []
+
+	for child in root:
+		name = child.find("name").text
+		if name is None: continue
+
+		xy   = get_location(child)
+		if xy is (None,None): continue
+
+		n    = child.find("n")
+		o 	 = child.find("o")
+		s    = child.find("s")
+		w    = child.find("w")
+
+		n    = parse_connection(n)
+		o    = parse_connection(o)
+		s    = parse_connection(s)
+		w    = parse_connection(w)
+
+		node = mapping_node(name,xy,(n,o,s,w))
+		map.append(node)
+
+	return map
+
+
+###############CANVAS######################
 size = 50
 line_width = 10
 
+width  = 800
+height = 800
+
+def convert_coordinates(location):
+	coordinates = copy.deepcopy(location)
+	x = (coordinates.x * size) + (width  / 2)
+	y = (coordinates.y * size) + (height / 2)
+	return x,y
+
+
 def canvas(map):
-	master = Tk()
-	width  = 800
-	height = 800
-	canv = Canvas(master,width = width,height = height)
+	master = tk.Tk()
+	canv = tk.Canvas(master,width = width,height = height)
 	canv.pack()
 	for element in map:
-		coordinates = copy.deepcopy(element.loc)
-		x = (coordinates.x * size) + (width  / 2)
-		y = (coordinates.y * size) + (height / 2)
+		x,y = convert_coordinates(element.loc)
 		canv.create_rectangle(x,y, (x+size), (y+size), fill = "red")
 		canv.create_text(x + 10,y + 10, text = element.name)
 		if element.connections.o.is_empty() is False:
-			coordinates2 = copy.deepcopy(element.connections.o)
-			x2 = (coordinates2.x * size) + (width / 2)
-			y2 = (coordinates2.y * size) + (height / 2)
+			x2,y2 = convert_coordinates(element.connections.o)
 			canv.create_line(x+size+1,y+10,x2,y2+10, fill="yellow", width = line_width)
 		if element.connections.w.is_empty() is False:
-			coordinates2 = copy.deepcopy(element.connections.w)
-			x2 = (coordinates2.x * size) + (width / 2)
-			y2 = (coordinates2.y * size) + (height / 2)
+			x2,y2 = convert_coordinates(element.connections.w)
 			canv.create_line(x,y+size-10,x2+size+1,y2+size-10, fill="orange", width = line_width)
 		if element.connections.n.is_empty() is False:
-			coordinates2 = copy.deepcopy(element.connections.n)
-			x2 = (coordinates2.x * size) + (width / 2)
-			y2 = (coordinates2.y * size) + (height / 2)
+			x2,y2 = convert_coordinates(element.connections.n)
 			canv.create_line(x+10,y,x2+10,y2+size+1, fill="blue", width = line_width)
 		if element.connections.s.is_empty() is False:
-			coordinates2 = copy.deepcopy(element.connections.s)
-			x2 = (coordinates2.x * size) + (width / 2)
-			y2 = (coordinates2.y * size) + (height / 2)
+			x2,y2 = convert_coordinates(element.connections.s)
 			canv.create_line(x+size-10,y+size+1,x2+size-10,y2, fill="purple", width = line_width)
-	mainloop()
+	tk.mainloop()
+
+###############MAPPINGDATA##########################
+# DATATYPES -> CLASSES
 
 class location(object):
 	'''
@@ -57,7 +105,6 @@ class location(object):
 			self.empty = False
 			self.x = x
 			self.y = y
-
 
 	def make_empty(bool = True):
 		self.empty = bool
@@ -86,11 +133,8 @@ class mapping_node(object):
 		self.loc = location(xy[0],xy[1])
 		self.connections = node_connections(nosw[0],nosw[1],nosw[2],nosw[3])	
 
+#############################MAIN#######################
 if __name__=='__main__':
-	map = []
-	map.append( mapping_node("A",(0,0),((None,None),(3,0),(0,3),(None,None))) )
-	map.append( mapping_node("B",(3,0),((None,None),(None,None),(3,3),(map[0].loc.x, map[0].loc.y))) )
-	map.append( mapping_node("C",(0,3),((map[0].loc.x, map[0].loc.y),(3,3),(None,None),(None,None))))
-	map.append( mapping_node("D",(3,3),((map[1].loc.x, map[1].loc.y),(None,None),(None,None),(map[2].loc.x, map[2].loc.y))) )
+	map = parse_mapping_xml()
 	canvas(map)
 	
